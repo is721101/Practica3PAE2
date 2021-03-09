68f7ae668d4339faab01884e454fe0a05a4e0e8b
is721101 <79496989+is721101@users.noreply.github.com>
2021-03-08 22:34:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -58,5 +58,6 @@ app.use(function(err, req, res, next) {
     res.render('error');
  });
 
- app.listen(3000, () => console.log('Listening on port 3000'));
+const port=process.env.PORT||'3000'
+app.set("port",PORT);
 module.exports = app;
